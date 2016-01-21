112b3d7d3d0e03edace2b8bc41224b8c9986bb22
Hai Nguyen <aquarchitecture@gmail.com>
2016-01-21 15:27:00 -0800

    Revert "Generated on 01-21-2016"
    This reverts commit 52ac8cb9d378200c0fac3581f82fa89632e8ace8.

--- jazzy_mykit.sh
+++ jazzy_mykit.sh
@@ -1,23 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ $(basename $(pwd)) != "MyKit" ]]; then
-	echo "Only run on the project's root directory"
+	echo "Only run on the project's root directory."
 	exit 1
 fi
 
-# ensure a clean directory because new changes will be stashes to gh-pages branch
-read -p 'Ensure the working directory clearn! Continue the process? [y|n]: ' answer
-answer=$(tr '[:upper:]' '[:lower:]' <<< $answer)
-
-# exit early
-if [[ ${answer:0:1} == "n" ]]; then
-	echo 'Documentation generating process terminated!'
-	exit 1
-fi
-
-if [[ ${answer:0:1} != "y" ]]; then
-	echo 'Invalid answer! Jazzy terminated!'
-	exit 1
+if [[ ! -z $(git status -s) ]]; then
+   echo "Jazzy process requires a clean directory."
+   exit 1
 fi
 
 # generate documentation
@@ -33,15 +23,23 @@ jazzy \
    --skip-undocumented \
    --readme ~/Documents/X-code/MyKit/README.md \
 
+# clean up main
+rm -r build/
+
 # forward files into gh-pages branch
+git add docs/
+git stash
 git checkout gh-pages
+git stash pop
 
-# clean up directory
-rm -r build && mv docs/* . && rm -r docs
+# clean up gh-pages directory
+rsync -avr --remove-source-files docs/* . && rm -r docs
 
-# git on gh-pages
-git add .
-git commit -m "Generated on $(date "+%m-%d-%Y")"
-git push -u origin gh-pages
+# git with gh-pages
+if [[ ! -z $(git status -s) ]]; then
+   git add .
+   git commit -m "Generated on $(date "+%m-%d-%Y")"
+   git push -u origin gh-pages
+fi
 
 git checkout master
