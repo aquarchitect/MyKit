1bed341039e0f8de94d0a4655341a1ee7347e505
Hai Nguyen <aquarchitecture@gmail.com>
2016-02-23 14:07:49 -0800

    Added NSPredicate+

---
+++ MyKit/Extension/NSPredicate+.swift
@@ -0,0 +1,15 @@
+//
+//  NSPredicate+.swift
+//  MyKit
+//
+//  Created by Hai Nguyen on 2/23/16.
+//  
+//
+
+public func && (lhs: NSPredicate, rhs: NSPredicate) -> NSPredicate {
+    return NSCompoundPredicate(andPredicateWithSubpredicates: [lhs, rhs])
+}
+
+public func || (lhs: NSPredicate, rhs: NSPredicate) -> NSPredicate {
+    return NSCompoundPredicate(orPredicateWithSubpredicates: [lhs, rhs])
+}
