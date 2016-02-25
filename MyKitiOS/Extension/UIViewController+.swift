b4d548a75f3a1025b3f86186ba241f3abc307a1c
Hai Nguyen <aquarchitecture@gmail.com>
2016-02-24 18:25:02 -0800

    Alert controller self-dismissed

--- MyKitiOS/Extension/UIViewController+.swift
+++ MyKitiOS/Extension/UIViewController+.swift
@@ -9,13 +9,9 @@
 public extension UIViewController {
 
     public func showsAlert(title: String? = nil, message: String) {
-        let action = UIAlertAction(title: "OK", style: .Default) { _ in
-            self.dismissViewControllerAnimated(true, completion: nil)
-        }
-        
         UIAlertController(title: title, message: message, preferredStyle: .Alert).then {
-            $0.addAction(action)
             self.presentViewController($0, animated: true, completion: nil)
+            delay(0.5) { self.dismissViewControllerAnimated(true, completion: nil) }
         }
     }
 }
