c3cd232c71bd398d9628d04d7cd46010b013e3ae
Hai Nguyen <aquarchitecture@gmail.com>
2016-03-20 00:53:49 -0700

    Added NSScanner+

---
+++ MyKit/Extension/Foundation/NSScanner+.swift
@@ -0,0 +1,91 @@
+//
+//  NSScanner+.swift
+//  MyKit
+//
+//  Created by Hai Nguyen on 3/18/16.
+//  
+//
+
+public extension NSScanner {
+
+    // MARK: String
+
+    public func scanCharactersFrom(set: NSCharacterSet) -> String? {
+        var result: NSString? = ""
+        return self.scanCharactersFromSet(set, intoString: &result) ? result as? String : nil
+    }
+
+    public func scanUpToCharactersFrom(set: NSCharacterSet) -> String? {
+        var result: NSString? = ""
+        return self.scanUpToCharactersFromSet(set, intoString: &result) ? result as? String : nil
+    }
+
+    public func scanString(string: String) -> String? {
+        var result: NSString? = ""
+        return self.scanString(string, intoString: &result) ? result as? String : nil
+    }
+
+    public func scanUpToString(string: String) -> String? {
+        var result: NSString? = ""
+        return self.scanUpToString(string, intoString: &result) ? result as? String : nil
+    }
+
+    // MARK: Number
+
+    public func scanDouble() -> Double? {
+        var result: Double = 0
+        return self.scanDouble(&result) ? result : nil
+    }
+
+    public func scanFloat() -> Float? {
+        var result: Float = 0
+        return self.scanFloat(&result) ? result : nil
+    }
+
+    public func scanInt() -> Int? {
+        var result: Int = 0
+        return self.scanInteger(&result) ? result : nil
+    }
+
+    public func scanInt32() -> Int32? {
+        var result: Int32 = 0
+        return self.scanInt(&result) ? result : nil
+    }
+
+    public func scanInt64() -> Int64? {
+        var result: Int64 = 0
+        return self.scanLongLong(&result) ? result : nil
+    }
+
+    public func scanUInt64() -> UInt64? {
+        var result: UInt64 = 0
+        return self.scanUnsignedLongLong(&result) ? result : nil
+    }
+
+    public func scanDecimal() -> NSDecimal? {
+        var result = NSDecimal()
+        return self.scanDecimal(&result) ? result : nil
+    }
+
+    // MARK: Hext Number
+
+    public func scanHexDouble() -> Double? {
+        var result: Double = 0
+        return self.scanHexDouble(&result) ? result : nil
+    }
+
+    public func scanHexFloat() -> Float? {
+        var result: Float = 0
+        return self.scanHexFloat(&result) ? result : nil
+    }
+
+    public func scanHexUInt32() -> UInt32? {
+        var result: UInt32 = 0
+        return self.scanHexInt(&result) ? result : nil
+    }
+
+    public func scanHexUInt64() -> UInt64? {
+        var result: UInt64 = 0
+        return self.scanHexLongLong(&result) ? result : nil
+    }
+}
