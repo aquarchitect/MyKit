1b2ce379ee02cdf3155d82bf69346efbee052434
Hai Nguyen <aquarchitecture@gmail.com>
2016-06-12 19:16:47 -0700

    Removed website link

--- Sources/iOS/Extensions/UILabel+.swift
+++ Sources/iOS/Extensions/UILabel+.swift
@@ -1,7 +1,7 @@
 /*
  * UILabel+.swift
  *
- * Copyright (c) 2015–2016 Hai Nguyen (http://aquarchitect.github.io)
+ * Copyright (c) 2015–2016 Hai Nguyen
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
