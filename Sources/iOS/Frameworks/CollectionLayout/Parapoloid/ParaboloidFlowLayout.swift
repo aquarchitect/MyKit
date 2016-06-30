f9668e81bf549659d3bdcc1d92059216553e83ab
Hai Nguyen <aquarchitecture@gmail.com>
2016-06-29 20:20:08 -0700

    Fixed minors

--- Sources/iOS/Frameworks/CollectionLayout/Parapoloid/ParaboloidFlowLayout.swift
+++ Sources/iOS/Frameworks/CollectionLayout/Parapoloid/ParaboloidFlowLayout.swift
@@ -48,7 +48,7 @@ public class ParaboloidFlowLayout: SnappingFlowLayout {
     }
 
     public override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
-        return (super.layoutAttributesForItemAtIndexPath(indexPath) as? ParaboloidLayoutAttributes)?.then {
+        return (super.layoutAttributesForItemAtIndexPath(indexPath)?.copy() as? ParaboloidLayoutAttributes)?.then {
             guard let contentOffset = self.collectionView?.contentOffset else { return }
 
             let center = $0.center.shiftToCoordinate(contentOffset)
