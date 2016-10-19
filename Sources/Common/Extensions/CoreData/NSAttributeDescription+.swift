6008e163135869b6c5c0df7739cfa700179c8a2b
Hai Nguyen <aquarchitecture@gmail.com>
2016-10-18 17:07:04 -0700

    Extension/CoreData clear to support Swift 2.3

--- Sources/Common/Extensions/CoreData/NSAttributeDescription+.swift
+++ Sources/Common/Extensions/CoreData/NSAttributeDescription+.swift
@@ -10,10 +10,19 @@ import CoreData
 
 public extension NSAttributeDescription {
 
+#if swift(>=3.0)
     convenience init(name: String, type: NSAttributeType, isOptional: Bool = false) {
         self.init()
         self.name = name
         self.isOptional = isOptional
         self.attributeType = type
     }
+#else
+    convenience init(name: String, type: NSAttributeType, optional: Bool = false) {
+        self.init()
+        self.name = name
+        self.optional = optional
+        self.attributeType = type
+    }
+#endif
 }
--- Sources/Common/Extensions/CoreData/NSRelationshipDescription+.swift
+++ Sources/Common/Extensions/CoreData/NSRelationshipDescription+.swift
@@ -10,6 +10,7 @@ import CoreData
 
 public extension NSRelationshipDescription {
 
+#if swift(>=3.0)
     convenience init(name: String, destination: NSEntityDescription, range: CountableRange<Int> = .init(0...0)) {
         self.init()
         self.name = name
@@ -17,4 +18,13 @@ public extension NSRelationshipDescription {
         self.minCount = range.lowerBound
         self.maxCount = range.upperBound - 1
     }
+#else
+    convenience init(name: String, destination: NSEntityDescription, range: Range<Int> = .init(0...0)) {
+        self.init()
+        self.name = name
+        self.destinationEntity = destination
+        self.minCount = range.startIndex
+        self.maxCount = range.endIndex - 1
+    }
+#endif
 }
