998eb95060d273ef3ee866ec61397247ab758e9d
Hai Nguyen <aquarchitecture@gmail.com>
2017-02-15 13:22:15 -0800

    [tag][2.1.1] Simplify redux implementation

--- Sources/Common/Utilities/Redux.swift
+++ Sources/Common/Utilities/Redux.swift
@@ -10,108 +10,49 @@ import Foundation
 
 /**
  * The implementation of `Redux` takes a very different approach from a typical 
- * unidirectional architecture flow from web development. The idea is to encapsulate
- * the single apllication STATE mutation with systematic ACTIONs.
+ * unidirectional architecture flow from web development. The concept is essentially
+ * the same which is to encapsulate the application STATE mutation process with
+ * ACTIONs.
  *
  * The key different components are:
- * - The application STATE is no longer being store in the Redux object by default, 
- *   and encouraged to encapsulate inside a middleware (AppStore). This approach 
- *   provides a much flexibility in term of when to commit STATE changes based on
- *   actions.
- * - ACTIONs recorder is proved to be not only helpful in debugging mode but also
- *   calculate the redundant information of application STATE, i.e selected index.
- * - All of potential errors are directed to be handled in one of the middlewares.
- *   By positioning it first in the queue, state subscription not only catches
- *   errors from reducers but also from other middlewares.
+ * - MIDDLEWARE plays a significant role in this implementation such as action recorder,
+ *   state subscription, app store (state storage)...
+ * - The implementation is kept fundamentally very simple. STATE is not stored in
+ *   this object but rather in an independent MIDDLEWARE.
+ * - All of throwing errors from REDUCERs can be caught on the first MIDDLEWARE
+ *   of the queue.
  */
 open class Redux<State, Action> {
 
-    public typealias Reducer = (State, Action) -> Observable<State>
+    public typealias Reducer = (State, Action) throws -> State
     public typealias Dispatcher = (Action) throws -> Void
     public typealias Middleware = (State, @escaping Dispatcher) -> Dispatcher
 
-    private let input = Observable<(State, Action)>()
+    private let reducer: Reducer
+    private let middleware: Middleware
 
-    // one cycle = reducers + middlewares
-    private var cycles = AnyIterator<(TimeInterval, Action)>(EmptyIterator())
+    public init(reducer: @escaping Reducer, middlewares: [Middleware]) {
+        self.reducer = reducer
+        self.middleware = Redux.merge(middlewares)
+    }
 
-    public init(reducers: [Reducer], middlewares: [Middleware]) {
-        let reducer = Redux.merge(reducers)
-        let middleware = Redux.merge(middlewares)
+    public func dispatch(state: State, action: Action) {
+        let newState: State
+        let firstDispatch: Dispatcher
 
-        input.flatMapLatest { oldState, action in
-            reducer(oldState, action)
-                .onError { error in
-                    // redirect reducer errors to one of the middles
-                    try? middleware(oldState, { _ in throw error })(action)
-                }.onNext { newState in
-                    try? middleware(newState, { _ in })(action)
-                }
-        }.onNext { state in
-            self.updateNextCycle(of: state)
+        do {
+            newState = try reducer(state, action)
+            firstDispatch = { _ in }
+        } catch {
+            newState = state
+            firstDispatch = { _ in throw error }
         }
-    }
 
-    private func updateNextCycle(of state: State) {
-        guard let (interval, action) = cycles.next() else { return }
-
-        DispatchQueue.main.asyncAfter(
-            deadline: .now() + interval,
-            execute: { self.input.update((state, action)) }
-        )
-    }
-
-    /// The implementation mutates state on one action at a time (one cycle).
-    /// A cycle counts as one when passing through all of reducers
-    /// and middlewares.
-    ///
-    /// - Parameters:
-    ///   - state: initial state
-    ///   - actions: action sequence
-    open func dispatch<I: IteratorProtocol>(_ state: State, _ actions: I)
-    where I.Element == (TimeInterval, Action) {
-        cycles = AnyIterator(actions)
-        updateNextCycle(of: state)
-    }
-
-    open func dispatch<S: Sequence>(_ state: State, _ actions: S)
-    where S.Iterator.Element == Action {
-        let _actions = actions
-            .lazy
-            .map { (Double(0), $0) }
-            .makeIterator()
-
-        dispatch(state, _actions)
+        try? middleware(newState, firstDispatch)(action)
     }
 }
 
 public extension Redux {
-
-    static func merge(_ reducers: [Reducer]) -> Reducer {
-        /* 
-         * The implementation can start with an empty observable;
-         * however, it would add another uneccessary layer of
-         * obserable to the final merged reducer.
-         */
-        if let firstReducer = reducers.first {
-            return reducers.dropFirst().reduce(
-                firstReducer,
-                { result, reducer in
-                    { state, action in
-                        result(state, action).flatMap {
-                            reducer($0, action)
-                        }
-                    }
-                }
-            )
-        } else {
-            return { state, _ in .lift(state)}
-        }
-    }
-
-    static func merge(_ reducers: Reducer...) -> Reducer {
-        return merge(reducers)
-    }
 
     static func merge(_ middlewares: [Middleware]) -> Middleware {
         return { state, dispatch in
--- UnitTests/ReduxTests.swift
+++ UnitTests/ReduxTests.swift
@@ -15,13 +15,11 @@ final class ReduxTests: XCTestCase {
     fileprivate class SimpleRedux: Redux<String, Bool> {}
 
     fileprivate let reducer: SimpleRedux.Reducer = { state, action in
-        let observable = Observable<String>()
-
-        DispatchQueue.main.async {
-            observable.update(action ? .fulfill(state) : .reject(Exception.a))
+        if action {
+            return state
+        } else {
+            throw Exception.a
         }
-
-        return observable
     }
 
     fileprivate let middleware1: SimpleRedux.Middleware = { state, dispatch in
@@ -42,31 +40,6 @@ final class ReduxTests: XCTestCase {
                 XCTAssertEqual(error as? Exception, action ? .b : .a)
             }
         }
-    }
-}
-
-extension ReduxTests {
-
-    func testMergingReducersWithSuccess() {
-        let exepectation = self.expectation(description: #function)
-
-        SimpleRedux.merge(reducer, reducer)("Initial", true).onNext {
-            XCTAssertEqual($0, "Initial")
-            exepectation.fulfill()
-        }
-
-        waitForExpectations(timeout: 2) { XCTAssertNil($0) }
-    }
-
-    func testMergingReducersWithFailure() {
-        let exepectation = self.expectation(description: #function)
-
-        SimpleRedux.merge(reducer, reducer)("Initial", false).onError {
-            XCTAssertEqual($0 as? Exception, .a)
-            exepectation.fulfill()
-        }
-
-        waitForExpectations(timeout: 2) { XCTAssertNil($0) }
     }
 }
 
@@ -97,28 +70,10 @@ extension ReduxTests {
         }
 
         SimpleRedux(
-            reducers: [reducer],
+            reducer: reducer,
             middlewares: [middleware, middleware2, middleware1]
-        ).dispatch("Initial", [true])
+        ).dispatch(state: "Initial", action: true)
 
         waitForExpectations(timeout: 2) { XCTAssertNil($0) }
-    }
-
-    func testReduxCycles() {
-        let expectation = self.expectation(description: #function)
-
-        var count = 0
-        let middleware: SimpleRedux.Middleware = { state, dispatch in
-            return { action in
-                count != 2 ? (count += 1) : expectation.fulfill()
-            }
-        }
-
-        SimpleRedux(
-            reducers: [reducer],
-            middlewares: [middleware, middleware2, middleware1]
-        ).dispatch("Initial", [true, true, true])
-
-        waitForExpectations(timeout: 4) { XCTAssertNil($0) }
     }
 }
