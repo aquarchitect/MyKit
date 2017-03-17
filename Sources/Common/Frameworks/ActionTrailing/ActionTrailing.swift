70cdce4362ea0f18cb29f60a198f655a4f221a0d
Hai Nguyen <aquarchitecture@gmail.com>
2017-03-17 13:42:06 -0700

    Conform UIBarButtonItem with ActionTrailing

--- Sources/Common/Frameworks/ActionTrailing/ActionTrailing.swift
+++ Sources/Common/Frameworks/ActionTrailing/ActionTrailing.swift
@@ -14,11 +14,11 @@ public protocol ActionTrailing: class {}
 
 extension ActionTrailing {
 
-    func setAction(_ handle: @escaping (Self) -> Void) {
+    func setAction(_ handler: @escaping (Self) -> Void) {
         objc_setAssociatedObject(
             self,
             &GlobalToken,
-            ActionWrapper(handle),
+            ActionWrapper(handler),
             objc_AssociationPolicy.OBJC_ASSOCIATION_COPY_NONATOMIC
         )
     }
@@ -42,8 +42,8 @@ extension UIControl: ActionTrailing {}
 
 public extension ActionTrailing where Self: UIControl {
 
-    func addAction(_ handle: @escaping (Self) -> Void, for controlEvents: UIControlEvents) {
-        self.setAction(handle)
+    func addAction(_ handler: @escaping (Self) -> Void, for controlEvents: UIControlEvents) {
+        self.setAction(handler)
         self.addTarget(self, action: #selector(handleAction), for: controlEvents)
     }
 }
@@ -52,11 +52,23 @@ extension UIGestureRecognizer: ActionTrailing {}
 
 public extension ActionTrailing where Self: UIGestureRecognizer {
 
-    func addAction(_ handle: @escaping (Self) -> Void) {
-        self.setAction(handle)
+    func addAction(_ handler: @escaping (Self) -> Void) {
+        self.setAction(handler)
         self.addTarget(self, action: #selector(handleAction))
     }
 }
+
+extension UIBarButtonItem: ActionTrailing {}
+
+public extension ActionTrailing where Self: UIBarButtonItem {
+
+    func addAction(_ handler: @escaping (Self) -> Void) {
+        self.setAction(handler)
+        self.target = self
+        self.action = #selector(handleAction)
+    }
+}
+
 #elseif os(OSX)
 import AppKit
 
@@ -64,8 +76,8 @@ extension NSControl: ActionTrailing {}
 
 public extension ActionTrailing where Self: NSControl {
 
-    func addAction(_ handle: @escaping (Self) -> Void) {
-        self.setAction(handle)
+    func addAction(_ handler: @escaping (Self) -> Void) {
+        self.setAction(handler)
         self.action = #selector(handleAction)
         self.target = self
     }
@@ -75,8 +87,8 @@ extension NSMenuItem: ActionTrailing {}
 
 public extension ActionTrailing where Self: NSMenuItem {
 
-    func addAction(_ handle: @escaping (Self) -> Void) {
-        self.setAction(handle)
+    func addAction(_ handler: @escaping (Self) -> Void) {
+        self.setAction(handler)
         self.action = #selector(handleAction)
         self.target = self
     }
@@ -86,8 +98,8 @@ extension NSGestureRecognizer: ActionTrailing {}
 
 public extension ActionTrailing where Self: NSGestureRecognizer {
 
-    func addAction(_ handle: @escaping (Self) -> Void) {
-        self.setAction(handle)
+    func addAction(_ handler: @escaping (Self) -> Void) {
+        self.setAction(handler)
         self.action = #selector(handleAction)
         self.target = self
     }
