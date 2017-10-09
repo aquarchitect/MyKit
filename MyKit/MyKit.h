e9bd416e56a79933901019dbf028cad1ceb61d5a
Hai Nguyen <aquarchitecture@gmail.com>
2017-10-09 13:05:09 -0700

    Refactor header files

---
+++ MyKit/MyKit.h
@@ -0,0 +1,15 @@
+//
+//  MyKit.h
+//  MyKit
+//
+//  Created by Hai Nguyen.
+//
+
+@import Foundation;
+
+//! Project version number for MyKit.
+FOUNDATION_EXPORT double MyKitVersionNumber;
+
+//! Project version string for MyKit.
+FOUNDATION_EXPORT const unsigned char MyKitVersionString[];
+
