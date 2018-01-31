89d6b8314456bea23d80acb075fd0f87845224f0
Hai Nguyen <aquarchitecture@gmail.com>
2018-01-31 13:45:56 -0800

    Update view constraints on an axis

--- Sources/iOS/Extensions/UIKit/UIView+.swift
+++ Sources/iOS/Extensions/UIKit/UIView+.swift
@@ -10,28 +10,41 @@ import UIKit
 
 public extension UIView {
 
-    func constraint(_ subviews: [UIView], equallyAlong axis: UILayoutConstraintAxis) {
+    func constraint(_ subviews: [UIView], equallyAlong axis: UILayoutConstraintAxis, spacing: CGFloat = 0) {
         var axisFormat: String = "\(axis.initial):|"
         var dictionaryViews: [String: UIView] = [:]
-        var oppositeFormat: [String] = []
 
         for (index, subview) in subviews.enumerated() {
             let key = "subview\(index)"
             dictionaryViews[key] = subview
 
-            oppositeFormat.append("\(axis.opposite.initial):|[\(key)]|")
-            axisFormat += "[" + key + (index == 0 ? "" : "(==subview0)") + "]"
+            if index == 0 {
+                axisFormat += "[" + key + "]"
+            } else if spacing == 0 {
+                axisFormat += "[" + key + "(==subview0)" + "]"
+            } else {
+                axisFormat += "-\(spacing)-[" + key + "(==subview0)" + "]"
+            }
         }
 
         axisFormat += "|"
 
-        (oppositeFormat + [axisFormat]).reduce([]) {
-            $0 + NSLayoutConstraint.constraints(
-                withVisualFormat: $1,
+        switch axis {
+        case .horizontal:
+            NSLayoutConstraint.constraints(
+                withVisualFormat: axisFormat,
+                options: [.alignAllTop, .alignAllBottom],
                 metrics: nil,
                 views: dictionaryViews
-            )
-        }.activate()
+            ).activate()
+        case .vertical:
+            NSLayoutConstraint.constraints(
+                withVisualFormat: axisFormat,
+                options: [.alignAllLeft, .alignAllRight],
+                metrics: nil,
+                views: dictionaryViews
+            ).activate()
+        }
     }
 }
 
@@ -41,13 +54,6 @@ fileprivate extension UILayoutConstraintAxis {
         switch self {
         case .horizontal: return "H"
         case .vertical: return "V"
-        }
-    }
-
-    var opposite: UILayoutConstraintAxis {
-        switch self {
-        case .horizontal: return .vertical
-        case .vertical: return .horizontal
         }
     }
 }
